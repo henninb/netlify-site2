77330e661195873bfcc56aaba8f57f6308080fcc
Brian Henning <henninb@msn.com>
2026-04-23 05:28:55 -0500

    wip

--- functions/vue-ssr.js
+++ functions/vue-ssr.js
@@ -1,45 +1,29 @@
-// Step 1: Create a Vue instance!
-import Vue from 'vue'
-import serverRenderer from 'vue-server-renderer'
+import { createSSRApp, h } from 'vue'
+import { renderToString } from '@vue/server-renderer'
 
-console.log(serverRenderer.default)
-
-const app = new Vue({
-  template: `<div>Hello World</div>`
-})
-
-const template = `<html>
+const template = (title, body) => `<html>
   <head>
-    <title>{{ title }}</title>
+    <title>${title}</title>
   </head>
   <body>
-    <!--vue-ssr-outlet-->
+    ${body}
   </body>
 </html>`
 
-// Step 2: Create a renderer
-const renderer = serverRenderer.createRenderer({
-  // template: require('fs').readFileSync('./index.template.html', 'utf-8')
-  template: template
-})
-
-exports.handler = (event, context, callback) => {
-  const ssrContext = {
-    title: 'Vue SSR Serverless'
-  }
-
-  // Step 3: Render the Vue instance to HTML
-  renderer.renderToString(app, ssrContext, (err, html) => {
-    if (err) throw err
-
-    const response = {
-      statusCode: 200,
-      headers: {
-        'Content-Type': 'text/html',
-      },
-      body: html
+exports.handler = async (event, context) => {
+  const app = createSSRApp({
+    render() {
+      return h('div', 'Hello World')
     }
-
-    return callback(null, response)
   })
+
+  const body = await renderToString(app)
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'text/html',
+    },
+    body: template('Vue SSR Serverless', body)
+  }
 }
